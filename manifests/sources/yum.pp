8036cdaa38010d6d30fe168dd9fd3985e93b50d0
ActionJack <martin@uncommonsense-uk.com>
2013-06-20 13:23:15 +0200

    Changed name to descr.

--- manifests/sources/yum.pp
+++ manifests/sources/yum.pp
@@ -1,6 +1,6 @@
 class mongodb::sources::yum inherits mongodb::params {
   yumrepo { '10gen':
-    name      => '10gen repo',
+    descr     => '10gen repo',
     baseurl   => $mongodb::params::baseurl,
     gpgcheck  => '0',
     enabled   => '1',
