6266ff671ba2a53e3ed0f5a7f6875e8c09b4073e
Tom McLaughlin <tmclaughlin@hubspot.com>
2013-07-02 12:48:42 -0400

    Fix user group and log directories for 10gen/local
    Closes #35
    
    Handle logpath correctly based on packages being used.  Also ensure that
    logpath is created.
    
    Fix dbpath based on packages being used.  Also make sure the path is
    created.
    
    Ensure dbpath and log directory exist before service starts.
    
    Add dependency on stdlib >= 2.2.0
    
    puppet-lint fixes

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,6 +6,11 @@ class mongodb::params{
       $package = 'mongodb-server'
       $service = 'mongod'
       $pkg_10gen = 'mongo-10gen-server'
+
+      $mongo_user_10gen = 'mongod'
+      $mongo_group_10gen = 'mongod'
+      $dbpath_10gen = '/var/lib/mongo/'
+      $logpath_10gen = '/var/log/mongo/mongod.log'
     }
     'debian': {
       $locations = {
@@ -20,9 +25,22 @@ class mongodb::params{
       $package = 'mongodb'
       $service = 'mongodb'
       $pkg_10gen = 'mongodb-10gen'
+
+      $mongo_user_10gen = 'mongodb'
+      $mongo_group_10gen = 'mongodb'
+      $dbpath_10gen = '/var/lib/mongodb/'
+      $logpath_10gen = '/var/log/mongodb/mongodb.log'
     }
     default: {
       fail ("mongodb: ${::operatingsystem} is not supported.")
     }
   }
+
+  $mongo_user_os = 'mongodb'
+  $mongo_group_os = 'mongodb'
+
+  $dbpath_os = '/var/lib/mongodb/'
+
+  $logpath_os = '/var/log/mongodb/mongodb.log'
+
 }
