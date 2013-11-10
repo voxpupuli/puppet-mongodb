97b17a9766cb5bad6f5645b938881a9230f42580
Dejan Golja <dejan@golja.org>
2013-11-11 01:18:12 +1100

    Refactor mongodb server.
    Based on the pattern design from puppetlabs modules I
    added some new classes to make the  functionality more self-contained
    (mongodb::server,  mongodb::install, mongodb::config, mongodb::repo, etc).
    
    Also the major change is the introduction of the mongodb::globals
    class which should be used for node/server specific settings,
    which could be the same for various mongod instances on the same server.
    
    The main mongodb::server class has similar API than the current mongodb,
    with extra parameters to cover new mongod functionality.
    
    New parameters:
     - verbositylevel
     - quotafiles
     - diaglog
     - directoryperdb
     - profile
    
    Furthermore the mongodb class was modified to use the new mongodb::server
    class so that the current module users who uses the default parameters
    values should be able to upgrade without any changes. More testing
    needed.

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,19 +30,22 @@
 #
 # Copyright 2012 PuppetLabs
 #
+
 class mongodb (
-  $enable_10gen    = false,
-  $init            = $mongodb::params::init,
+  # Deprecated parameters
+  $enable_10gen    = undef,
+
+  $init            = $mongodb::params::service_provider, 
   $location        = '',
   $packagename     = undef,
   $version         = undef,
-  $servicename     = $mongodb::params::service,
-  $service_enable  = true,
-  $logpath         = undef,
+  $servicename     = $mongodb::params::service_name,
+  $service_enable  = true, #deprecated
+  $logpath         = $mongodb::params::logpath,
   $logappend       = true,
-  $fork            = true,
+  $fork            = $mongodb::params::fork,
   $port            = '27017',
-  $dbpath          = undef,
+  $dbpath          = $mongodb::params::dbpath,
   $journal         = undef,
   $nojournal       = undef,
   $smallfiles      = undef,
@@ -52,7 +55,7 @@ class mongodb (
   $verbose         = undef,
   $objcheck        = undef,
   $quota           = undef,
-  $oplog           = undef,
+  $oplog           = undef, #deprecated it's on if replica set
   $oplog_size      = undef,
   $nohints         = undef,
   $nohttpinterface = undef,
@@ -71,145 +74,61 @@ class mongodb (
   $rest            = undef,
   $slowms          = undef,
   $keyfile         = undef,
-  $bind_ip          = undef,
+  $bind_ip         = undef,
   $pidfilepath     = undef
 ) inherits mongodb::params {
+
   if $enable_10gen {
-    include $mongodb::params::source
-    Class[$mongodb::params::source] -> Package['mongodb-10gen']
-
-    $mongo_user = $mongodb::params::mongo_user_10gen
-    $mongo_group = $mongodb::params::mongo_group_10gen
-    $config_path = $mongodb::params::config_path_10gen
-  } else {
-    $mongo_user = $mongodb::params::mongo_user_os
-    $mongo_group = $mongodb::params::mongo_group_os
-    $config_path = '/etc/mongodb.conf'
-  }
-
-  if $dbpath == undef {
-    if $enable_10gen {
-      $_dbpath = $mongodb::params::default_dbpath_10gen
-    } else {
-      $_dbpath = $mongodb::params::default_dbpath
-    }
-  } else {
-    $_dbpath = $dbpath
-  }
-
-  if $logpath == undef {
-    if $enable_10gen {
-      $_logpath = $mongodb::params::default_logpath_10gen
-    } else {
-      $_logpath = $mongodb::params::default_logpath
-    }
-  } else {
-    $_logpath = $logpath
-  }
-
-  if $pidfilepath == undef {
-    if $enable_10gen {
-      $_pidfilepath = $mongodb::params::default_pidfilepath_10gen
-    } else {
-      $_pidfilepath = $mongodb::params::default_pidfilepath
-    }
-  } else {
-    $_pidfilepath = $pidfilepath
-  }
-
-  if $bind_ip == undef {
-    if $enable_10gen {
-      $_bind_ip = $mongodb::params::default_bind_ip_10gen
-    } else {
-      $_bind_ip = $mongodb::params::default_bind_ip
-    }
-  } else {
-    $_bind_ip = $bind_ip
-  }
-
-  if $fork == undef {
-    if $enable_10gen {
-      $_fork = $mongodb::params::default_fork_10gen
-    } else {
-      $_fork = $mongodb::params::default_fork
-    }
-  } else {
-    $_fork = $fork
-  }
-
-  if $journal == undef {
-    if $enable_10gen {
-      $_journal = $mongodb::params::default_journal_10gen
-    } else {
-      $_journal = $mongodb::params::default_journal
-    }
-  } else {
-    $_journal = $journal
-  }
-
-  # NOTE: dirname() not available until stdlib 4.1.0
-  $logpath_array = split($_logpath, '/')
-  $logpath_dir_array = delete_at($logpath_array, -1)
-  $logpath_dir = join($logpath_dir_array, '/')
-
-
-  if $packagename {
-    $package = $packagename
-  } elsif $enable_10gen {
-    $package = $mongodb::params::pkg_10gen
-  } else {
-    $package = $mongodb::params::package
+    fail("Parameter enable_10gen not supported. Please use class { 'mongodb::globals': manage_package_repo => true }")
   }
 
   if $version {
-    $ensure_package = $version
-  } else {
-    $ensure_package = installed
+    fail("Parameter version is deprecated. Please use class { 'mongodb::globals': version => VERSION }")
+  }
+  
+  if $oplog {
+    fail("Parameter is deprecated. On replica set Oplog is enabled by default.")
+  }
+  
+  notify { "An attempt has been made below to automatically apply your custom
+    settings to mongodb::server. Please verify this works in a safe test
+    environment.": }
+
+  class { 'mongodb::server':
+    package_name    => $packagename,
+    logpath         => $logpath,
+    logappend       => $logappend,
+    fork            => $fork,
+    port            => $port,
+    dbpath          => $dbpath,
+    journal         => $journal,
+    nojournal       => $nojournal,
+    smallfiles      => $smallfiles,
+    cpu             => $cpu,
+    noauth          => $noauth,
+    verbose         => $verbose,
+    objcheck        => $objcheck,
+    quota           => $quota,
+    oplog_size      => $oplog_size,
+    nohints         => $nohints,
+    nohttpinterface => $nohttpinterface,
+    noscripting     => $noscripting,
+    notablescan     => $notablescan,
+    noprealloc      => $noprealloc,
+    nssize          => $nssize,
+    mms_token       => $mms_token,
+    mms_name        => $mms_name,
+    mms_interval    => $mms_interval,
+    slave           => $slave,
+    only            => $only,
+    master          => $master,
+    source          => $source,
+    replset         => $replset,
+    rest            => $rest,
+    slowms          => $slowms,
+    keyfile         => $keyfile,
+    bind_ip         => $bind_ip,
+    pidfilepath     => $pidfilepath,
   }
 
-  package { 'mongodb-10gen':
-    name   => $package,
-    ensure => $ensure_package,
-  }
-
-  file { $_dbpath:
-    ensure  => directory,
-    owner   => $mongo_user,
-    group   => $mongo_group,
-    mode    => '0755',
-    require => Package['mongodb-10gen']
-  }
-
-  file { $logpath_dir:
-    ensure  => directory,
-    owner   => $mongo_user,
-    group   => $mongo_group,
-    mode    => '0755',
-    require => Package['mongodb-10gen']
-  }
-
-  file { $config_path:
-    content => template('mongodb/mongodb.conf.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Package['mongodb-10gen'],
-  }
-
-  validate_bool($service_enable)
-  if $service_enable {
-    $service_ensure = 'running'
-    $service_subscribe = File[$config_path]
-  } else {
-    $service_ensure = 'stopped'
-    $service_subscribe = undef
-  }
-
-  service { 'mongodb':
-    name      => $servicename,
-    ensure    => $service_ensure,
-    enable    => $service_enable,
-    subscribe => $service_subscribe,
-    require   => [File[$_dbpath], File[$logpath_dir]]
-  }
 }
