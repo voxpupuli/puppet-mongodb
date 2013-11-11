12cf1c6bac24075561e8df72865cfc84f1ee73b1
Ashley Penney <ashley.penney@puppetlabs.com>
2013-11-11 18:35:25 -0500

    Improvements to the tests.

--- spec/system/mongodb_service_spec.rb
+++ spec/system/mongodb_service_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper_system'
 
 describe 'mongodb::service' do
 
+  case node.facts['osfamily']
+  when 'RedHat'
+    service_name = 'mongod'
+  else
+    service_name = 'mongodb'
+  end
+
   it 'runs setup' do
     pp = <<-EOS
     class { 'mongodb': }
@@ -9,7 +16,7 @@ describe 'mongodb::service' do
     puppet_apply(pp)
   end
 
-  describe service('mongodb') do
+  describe service(service_name) do
     it { should be_enabled }
     it { should be_running }
   end
