70b018483463f52d0b8d20921a0426d01a875d92
Hunter Haugen <hunter@puppetlabs.com>
2014-05-16 17:15:33 -0400

    Add tests for replsets

---
+++ spec/acceptance/server_spec.rb
@@ -0,0 +1,131 @@
+require 'spec_helper_acceptance'
+
+describe 'mongodb::server class' do
+
+  it { require'pry';binding.pry }
+
+  shared_example 'normal tests' do |tengen|
+    if tengen
+      case fact('osfamily')
+      when 'RedHat'
+        package_name = 'mongo-10gen-server'
+        service_name = 'mongod'
+        config_file  = '/etc/mongod.conf'
+      when 'Debian'
+        package_name = 'mongodb-10gen'
+        service_name = 'mongodb'
+        config_file  = '/etc/mongodb.conf'
+      end
+    else
+      case fact('osfamily')
+      when 'RedHat'
+        package_name = 'mongodb-server'
+        service_name = 'mongod'
+        config_file  = '/etc/mongodb.conf'
+      when 'Debian'
+        package_name = 'mongodb-server'
+        service_name = 'mongodb'
+        config_file  = '/etc/mongodb.conf'
+      end
+    end
+
+    client_name  = 'mongo --version'
+
+    context "default parameters with 10gen => #{tengen}" do
+      it 'should work with no errors' do
+        case fact('osfamily')
+        when 'RedHat'
+          pp = <<-EOS
+            class { 'mongodb::globals': manage_package_repo => #{tengen}, }
+            -> class { 'mongodb::server': }
+            -> class { 'mongodb::client': }
+          EOS
+        when 'Debian'
+          pp = <<-EOS
+            class { 'mongodb::globals': manage_package_repo => #{tengen}, }
+            -> class { 'mongodb::server': }
+          EOS
+        end
+
+        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp, :catch_changes  => true)
+      end
+
+      describe package(package_name) do
+        it { should be_installed }
+      end
+
+      describe file(config_file) do
+        it { should be_file }
+      end
+
+      describe service(service_name) do
+         it { should be_enabled }
+         it { should be_running }
+      end
+
+      describe port(27017) do
+        it do
+          sleep(20)
+          should be_listening
+        end
+      end
+
+      describe command(client_name) do
+        it do
+          should return_exit_status 0
+        end
+      end
+    end
+
+    context "test using custom port and 10gen => #{tengen}" do
+      it 'change port to 27018' do
+        pp = <<-EOS
+          class { 'mongodb::globals': manage_package_repo => #{tengen}, }
+          -> class { 'mongodb::server': port => 27018, }
+        EOS
+
+        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp, :catch_changes  => true)
+      end
+
+      describe port(27018) do
+        sleep(20)
+        it { should be_listening }
+      end
+    end
+
+    context "test shutdown of custom port with 10gen => #{tengen}" do
+      it 'shut down service on port 27018' do
+        pp = <<-EOS
+          class {'mongodb::globals': manage_package_repo => #{tengen}, }
+          -> class {'mongodb::server':
+            ensure => absent,
+            port   => 27018,
+          }
+        EOS
+
+        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp, :catch_changes  => true)
+      end
+
+      describe port(27018) do
+        it { should_not be_listening}
+      end
+    end
+
+    describe "uninstalling with 10gen => #{tengen}" do
+      it 'uninstalls mongodb' do
+        pp = <<-EOS
+          class {'mongodb::globals': manage_package_repo => #{tengen}, }
+          -> class { 'mongodb::server': ensure => absent, }
+          -> class { 'mongodb::client': ensure => absent, }
+        EOS
+        apply_manifest(pp, :catch_failures => true)
+      end
+    end
+  end
+
+  it_behaves_like 'normal tests', false
+  it_behaves_like 'normal tests', true
+end
