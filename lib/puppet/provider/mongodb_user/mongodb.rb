d3121775932be5ac8e286628ed601e8e99b2bdb7
Bin Long <binlong.plus@gmail.com>
2014-12-12 14:58:58 +0800

    Fix the bug that new user's password_hash will be rehashed

--- lib/puppet/provider/mongodb_user/mongodb.rb
+++ lib/puppet/provider/mongodb_user/mongodb.rb
@@ -64,14 +64,15 @@ Puppet::Type.type(:mongodb_user).provide(:mongodb, :parent => Puppet::Provider::
 
       mongo_eval("db.addUser(#{user.to_json})", @resource[:database])
     else
-      user = {
-        :user => @resource[:username],
+      cmd = {
+        :createUser => @resource[:username],
         :pwd => @resource[:password_hash],
         :customData => { :createdBy => "Puppet Mongodb_user['#{@resource[:name]}']" },
-        :roles => @resource[:roles]
+        :roles => @resource[:roles],
+        :digestPassword => false,
       }
 
-      mongo_eval("db.createUser(#{user.to_json})", @resource[:database])
+      mongo_eval("db.runCommand(#{cmd.to_json})", @resource[:database])
     end
 
     @property_hash[:ensure] = :present
