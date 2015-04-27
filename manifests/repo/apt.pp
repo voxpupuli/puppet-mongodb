c392f30f886a63915a7ba1a458d049f0d15bac4a
Mark Wilde <mwilde@ingenium-ids.org>
2015-04-27 17:04:15 +0100

    Updated apt::source params
    Changed the params to support recent changes in the apt module:
    
    https://github.com/puppetlabs/puppetlabs-apt/commit/ea4f615735d5fb6bb6b0fcbd735da76e0bb45c06

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -7,12 +7,16 @@ class mongodb::repo::apt inherits mongodb::repo {
 
   if($::mongodb::repo::ensure == 'present' or $::mongodb::repo::ensure == true) {
     apt::source { 'downloads-distro.mongodb.org':
-      location    => $::mongodb::repo::location,
-      release     => 'dist',
-      repos       => '10gen',
-      key         => '492EAFE8CD016A07919F1D2B9ECBEC467F0CEB10',
-      key_server  => 'hkp://keyserver.ubuntu.com:80',
-      include_src => false,
+      location => $::mongodb::repo::location,
+      release  => 'dist',
+      repos    => '10gen',
+      key      => {
+        'id'     => '492EAFE8CD016A07919F1D2B9ECBEC467F0CEB10',
+        'server' => 'hkp://keyserver.ubuntu.com:80',
+      },
+      include  => {
+        'src' => false,
+      },
     }
 
     Apt::Source['downloads-distro.mongodb.org']->Package<|tag == 'mongodb'|>
