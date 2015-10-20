715c6e808e3601b71e85594aef0c27f2fc5a40b9
Guillaume Penaud <gpenaud@xilopix.com>
2015-10-20 14:41:23 +0200

    [hiera] - make hiera usable with databases configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -81,7 +81,8 @@ class mongodb (
   $key             = undef,
   $ipv6            = undef,
   $bind_ip         = undef,
-  $pidfilepath     = undef
+  $pidfilepath     = undef,
+  $databases       = {}
 ) inherits mongodb::params {
 
   if $enable_10gen {
@@ -142,4 +143,6 @@ class mongodb (
     pidfilepath     => $pidfilepath,
   }
 
+  # to make hiera usable with databases configuration
+  create_resources('mongodb::db', $databases)
 }
