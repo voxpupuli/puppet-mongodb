48deb60919539a7e4aef9c3f88615f721bb13cbd
Matt Simmons <matthew.simmons@spacex.com>
2015-12-15 08:21:56 -0800

    Fixing some arrow alignment

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -7,15 +7,15 @@ class mongodb::repo::apt inherits mongodb::repo {
 
   if($::mongodb::repo::ensure == 'present' or $::mongodb::repo::ensure == true) {
     apt::source { 'mongodb':
-      location   => $::mongodb::repo::location,
-      release    => 'dist',
-      repos      => '10gen',
-      key        => {
+      location => $::mongodb::repo::location,
+      release  => 'dist',
+      repos    => '10gen',
+      key      => {
         'id'     => '492EAFE8CD016A07919F1D2B9ECBEC467F0CEB10',
         'server' => 'hkp://keyserver.ubuntu.com:80',
       },
-      include    => {
-        'src'    => false
+      include  => {
+        'src' => false
       }
     }
 
