227de42e7e3b8ce6c660d0a87c921729034c4199
Gavin Williams <fatmcgav@gmail.com>
2016-03-16 14:57:39 +0000

    (MODULES-3170) Add  param to allow unique resource titles when exporting the resource

--- manifests/db.pp
+++ manifests/db.pp
@@ -5,6 +5,7 @@
 # == Parameters
 #
 #  user - Database username.
+#  db_name - Database name. Defaults to $name.
 #  password_hash - Hashed password. Hex encoded md5 hash of "$username:mongo:$password".
 #  password - Plain text user password. This is UNSAFE, use 'password_hash' unstead.
 #  roles (default: ['dbAdmin']) - array with user roles.
@@ -12,13 +13,14 @@
 #
 define mongodb::db (
   $user,
+  $db_name       = $name,
   $password_hash = false,
   $password      = false,
   $roles         = ['dbAdmin'],
   $tries         = 10,
 ) {
 
-  mongodb_database { $name:
+  mongodb_database { $db_name:
     ensure => present,
     tries  => $tries
   }
@@ -31,13 +33,13 @@ define mongodb::db (
     fail("Parameter 'password_hash' or 'password' should be provided to mongodb::db.")
   }
 
-  mongodb_user { "User ${user} on db ${name}":
+  mongodb_user { "User ${user} on db ${db_name}":
     ensure        => present,
     password_hash => $hash,
     username      => $user,
-    database      => $name,
+    database      => $db_name,
     roles         => $roles,
-    require       => Mongodb_database[$name],
+    require       => Mongodb_database[$db_name],
   }
 
 }
