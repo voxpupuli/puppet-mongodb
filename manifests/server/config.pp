b9f97ea571f55ed18439d2f9363f4747ae219f18
Maksim Malchuk <maksim.malchuk@gmail.com>
2016-06-01 22:18:22 +0300

    Recursively manage the contents of dbpath directory
    Retain idempotency and manage all files recursively when
    mongodb::server used on dbpath directory already containing the
    data.
    
    Signed-off-by: Maksim Malchuk <maksim.malchuk@gmail.com>

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -207,11 +207,14 @@ class mongodb::server::config {
     }
 
     file { $dbpath:
-      ensure  => directory,
-      mode    => '0755',
-      owner   => $user,
-      group   => $group,
-      require => File[$config],
+      ensure   => directory,
+      mode     => '0755',
+      owner    => $user,
+      group    => $group,
+      recurse  => true,
+      purge    => false,
+      checksum => 'none',
+      require  => File[$config],
     }
 
     if $pidfilepath {
