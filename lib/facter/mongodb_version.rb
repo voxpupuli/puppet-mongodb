562cbabef684573179746f4c057ec4999df9809e
Liss Tarnell <ft@le-Fay.ORG>
2016-12-05 17:39:57 +0000

    fix version detection for MongoDB 3.x
    2.x output:
    	MongoDB shell version: 2.4.8
    
    3.x output:
    	MongoDB shell version v3.4.0

--- lib/facter/mongodb_version.rb
+++ lib/facter/mongodb_version.rb
@@ -2,7 +2,7 @@ Facter.add(:mongodb_version) do
   setcode do
     if Facter::Core::Execution.which('mongo')
       mongodb_version = Facter::Core::Execution.execute('mongo --version 2>&1')
-      %r{^MongoDB shell version: ([\w\.]+)}.match(mongodb_version)[1]
+      %r{^MongoDB shell version(: | v)([\w\.]+)}.match(mongodb_version)[2]
     end
   end
 end
