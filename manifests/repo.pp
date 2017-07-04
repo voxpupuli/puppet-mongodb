2070b1b3e6e293a4745c0158681741e6d0ae36d6
Bram Vogelaar <attachmentgenie@inuits.eu>
2017-07-04 10:08:03 +0200

    #361 adding requested check for when use_enterprise is selected, which means version needs to be set, moved the version split out of if tree to prevent code duplication

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -9,16 +9,17 @@ class mongodb::repo (
 ) inherits mongodb::params {
   case $::osfamily {
     'RedHat', 'Linux': {
+      if $version != undef {
+        $mongover = split($version, '[.]')
+      }
       if ($repo_location != undef){
         $location = $repo_location
         $description = 'MongoDB Custom Repository'
       } elsif $mongodb::globals::use_enterprise_repo == true {
-        $mongover = split($version, '[.]')
         $location = "https://repo.mongodb.com/yum/redhat/\$releasever/mongodb-enterprise/${mongover[0]}.${mongover[1]}/\$basearch/"
         $description = 'MongoDB Enterprise Repository'
       }
       elsif $version and (versioncmp($version, '3.0.0') >= 0) {
-        $mongover = split($version, '[.]')
         $location = $::architecture ? {
           'x86_64' => "http://repo.mongodb.org/yum/redhat/${::operatingsystemmajrelease}/mongodb-org/${mongover[0]}.${mongover[1]}/x86_64/",
           default  => undef
