84cd577cc229b66c32a938f3a090da5c84aacf82
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-11-08 22:22:09 +0100

    Simplify the client class
    The separate install class provided no benefit and only made it harder
    to follow.

--- spec/classes/client_spec.rb
+++ spec/classes/client_spec.rb
@@ -8,6 +8,15 @@ describe 'mongodb::client' do
       context 'with defaults' do
         it { is_expected.to compile.with_all_deps }
       end
+
+      context 'with manage_package' do
+        let(:pre_condition) do
+          "class { 'mongodb::globals': manage_package => true }"
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_package('mongodb_client').with_ensure('present') }
+      end
     end
   end
 end
