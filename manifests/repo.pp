46a75ebc7439079042f69a4f35cfe68bbf9e570e
Remko van Hunen <remko@imgzine.com>
2018-06-04 14:59:56 +0200

    This will add the correct apt key for version 3.6. Fix issue #465

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -65,6 +65,7 @@ class mongodb::repo (
           default => undef
         }
         $key = "${mongover[0]}.${mongover[1]}" ? {
+          '3.6'   => '2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5',
           '3.4'   => '0C49F3730359A14518585931BC711F9BA15703C6',
           '3.2'   => '42F3E95A2C4F08279C4960ADD68FA50FEA312927',
           default => '492EAFE8CD016A07919F1D2B9ECBEC467F0CEB10'
