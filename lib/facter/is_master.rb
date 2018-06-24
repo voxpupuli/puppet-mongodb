voxpupuli/puppet-mongodb

commit 991af660ffc34f5072e8973d895168956989f603

Handle non-existing config file
When using the RH MongoDB 3.4 SCL the config file is in
/etc/opt/rh/rh-mongodb34/mongod.conf which causes this fact to fail
because the -syspaths package still installs the binaries to /usr/bin.
Since we don't use this fact, it's enough for now to gracefully handle
this.

Ideally we could use the value for mongodb::server::config but I'm not
aware of a way to do this in facts.

lib/facter/is_master.rb
@@ -2,12 +2,8 @@ require 'json'
 require 'yaml'
 
 def mongod_conf_file
-  file = if File.exist? '/etc/mongod.conf'
-           '/etc/mongod.conf'
-         else
-           '/etc/mongodb.conf'
-         end
-  file
+  locations = ['/etc/mongod.conf', '/etc/mongodb.conf']
+  locations.find { |location| File.exist? location }
 end
 
 def get_options_from_hash_config(config)
@@ -53,16 +49,20 @@ Facter.add('mongodb_is_master') do
   setcode do
     if %w[mongo mongod].all? { |m| Facter::Util::Resolution.which m }
       file = mongod_conf_file
-      options = get_config_options(file)
-      e = File.exist?('/root/.mongorc.js') ? 'load(\'/root/.mongorc.js\'); ' : ''
+      if file
+        options = get_config_options(file)
+        e = File.exist?('/root/.mongorc.js') ? 'load(\'/root/.mongorc.js\'); ' : ''
 
-      # Check if the mongodb server is responding:
-      Facter::Core::Execution.exec("mongo --quiet #{options} --eval \"#{e}printjson(db.adminCommand({ ping: 1 }))\"")
+        # Check if the mongodb server is responding:
+        Facter::Core::Execution.exec("mongo --quiet #{options} --eval \"#{e}printjson(db.adminCommand({ ping: 1 }))\"")
 
-      if $CHILD_STATUS.success?
-        Facter::Core::Execution.exec("mongo --quiet #{options} --eval \"#{e}db.isMaster().ismaster\"")
+        if $CHILD_STATUS.success?
+          Facter::Core::Execution.exec("mongo --quiet #{options} --eval \"#{e}db.isMaster().ismaster\"")
+        else
+          'not_responding'
+        end
       else
-        'not_responding'
+        'not_configured'
       end
     else
       'not_installed'

commit cbcb8bd7572d280e533216ce3953dcf6f320b22d

Use the correct function in is_master fact
A regression introduced in 991af660ffc34f5072e8973d895168956989f603

lib/facter/is_master.rb
@@ -50,7 +50,7 @@ Facter.add('mongodb_is_master') do
     if %w[mongo mongod].all? { |m| Facter::Util::Resolution.which m }
       file = mongod_conf_file
       if file
-        options = get_config_options(file)
+        options = get_options_from_config(file)
         e = File.exist?('/root/.mongorc.js') ? 'load(\'/root/.mongorc.js\'); ' : ''
 
         # Check if the mongodb server is responding: