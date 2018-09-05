72ca446a1e0e1f63ec7f5186a775c12a8f2230aa
Tom Ritserveldt <muffin@inuits.be>
2018-09-05 09:42:57 +0200

    removed duplicate gsub that is already present in 'lib/puppet/provider/mongodb.rb'
    cleaned up the test json to pass valid json

--- lib/puppet/provider/mongodb_replset/mongo.rb
+++ lib/puppet/provider/mongodb_replset/mongo.rb
@@ -388,8 +388,6 @@ Puppet::Type.type(:mongodb_replset).provide(:mongo, parent: Puppet::Provider::Mo
       raise
     end
 
-    output.gsub!(%r{\w+\((.+?)\)}, '\1')
-
     # Hack to avoid non-json empty sets
     output = '{}' if output == "null\n"
     output = '{}' if output == "\nnull\n"
