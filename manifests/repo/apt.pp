8290f49f5c894dda57394982a856c4542e8c2521
Tim Meusel <tim@bastelfreak.de>
2019-04-06 20:25:49 +0200

    puppet-lint: fix relative_classname_inclusion

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -3,7 +3,7 @@ class mongodb::repo::apt inherits mongodb::repo {
   # we try to follow/reproduce the instruction
   # from http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
 
-  include ::apt
+  include apt
 
   if($mongodb::repo::ensure == 'present' or $mongodb::repo::ensure == true) {
     apt::source { 'mongodb':
