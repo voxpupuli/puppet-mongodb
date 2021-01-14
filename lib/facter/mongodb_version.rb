13f8abab2c0fdd4e11566f76745f89abbfb293e3
Kristof Willaert <kristof@publiq.be>
2021-01-14 15:29:08 +0100

    Use confine to check the existence of a command needed for the fact to resolve

--- lib/facter/mongodb_version.rb
+++ lib/facter/mongodb_version.rb
@@ -1,8 +1,8 @@
 Facter.add(:mongodb_version) do
+  confine { Facter::Core::Execution.which('mongo') }
+
   setcode do
-    if Facter::Core::Execution.which('mongo')
-      mongodb_version = Facter::Core::Execution.execute('mongo --version 2>&1')
-      %r{MongoDB shell version:?\s+v?([\w\.]+)}.match(mongodb_version)[1]
-    end
+    mongodb_version = Facter::Core::Execution.execute('mongo --version 2>&1')
+    %r{MongoDB shell version:?\s+v?([\w\.]+)}.match(mongodb_version)[1]
   end
 end
