9780a73e2e2851462163a2981be18428bddaed76
Evgeny Soynov <esoynov@kogito.network>
2021-05-12 11:35:38 +0200

    Improved mongo output handling This changes the way the module handles mongodb output: 1. it would not do any sanitization if the output already contains    a valid json. 2. it would try to extract a json object from a corrupted output 3. it would fallback to the previous logic if the json extraction failed

--- lib/puppet/util/mongodb_output.rb
+++ lib/puppet/util/mongodb_output.rb
@@ -1,15 +1,40 @@
+require 'json'
+
 module Puppet
   module Util
     module MongodbOutput
       def self.sanitize(data)
+        # If it already happily contains a valid json, do not do any sanitization
+        return data if is_parseable_json(data)
+
         # Dirty hack to remove JavaScript objects
         data.gsub!(%r{\w+\((\d+).+?\)}, '\1') # Remove extra parameters from 'Timestamp(1462971623, 1)' Objects
         data.gsub!(%r{\w+\((.+?)\)}, '\1')
+
+        # Probably theres a json object that we could extract from the output
+        maybe_json = try_extract_json(data)
+        return maybe_json unless maybe_json.nil?
 
         data.gsub!(%r{^Error\:.+}, '')
         data.gsub!(%r{^.*warning\:.+}, '') # remove warnings if sslAllowInvalidHostnames is true
         data.gsub!(%r{^.*The server certificate does not match the host name.+}, '') # remove warnings if sslAllowInvalidHostnames is true mongo 3.x
         data
+      end
+
+      def self.is_parseable_json(data)
+        !!JSON.parse(data)
+      rescue JSON::ParserError
+        false
+      end
+
+      def self.try_extract_json(data)
+        json_data = data.dup
+        unescaped_quotes = json_data.scan(%r{:\s*"(.*"+.*)"}).flatten
+        for str in unescaped_quotes do
+          json_data.sub!(str, str.gsub('"', '\"'))
+        end
+        maybe_json = json_data.gsub(%r{^[^{]*(?<json>{[\P{Cn}\P{Cs}]*})[^}]*$}, '\k<json>')
+        maybe_json if is_parseable_json(maybe_json)
       end
     end
   end
--- spec/unit/puppet/util/mongodb_output_spec.rb
+++ spec/unit/puppet/util/mongodb_output_spec.rb
@@ -53,6 +53,45 @@ describe Puppet::Util::MongodbOutput do
     EOT
   end
 
+  let(:corrupted_output) do
+    <<-EOT
+    Error: Authentication failed.
+    2021-05-11T15:35:19.647+0200 E QUERY    [thread1] Error: Could not retrieve replica set config: {
+    	"ok" : 0,
+    	"errmsg" : "not authorized on admin to execute command { replSetGetConfig: 1.0, $clusterTime: { clusterTime: Timestamp(0, 0), signature: { hash: BinData(0, 0000000000000000000000000000000000000000), keyId: 0 } }, $readPreference: { mode: \"secondaryPreferred\" }, $db: \"admin\" }",
+    	"code" : 13,
+    	"codeName" : "Unauthorized",
+    	"$clusterTime" : {
+    		"clusterTime" : Timestamp(0, 0),
+    		"signature" : {
+    			"hash" : BinData(0,"AAAAAAAAAAAAAAAAAAAAAAAAAAA="),
+    			"keyId" : NumberLong(0)
+    		}
+    	}
+    } :
+    rs.conf@src/mongo/shell/utils.js:1323:11
+    @(shell eval):1:43'
+    EOT
+  end
+
+  let(:corrected_corrupted_output) do
+    <<-EOT
+    {
+    	"ok" : 0,
+    	"errmsg" : "not authorized on admin to execute command { replSetGetConfig: 1.0, $clusterTime: { clusterTime: 0, signature: { hash: 0, keyId: 0 } }, $readPreference: { mode: \\"secondaryPreferred\\" }, $db: \\"admin\\" }",
+    	"code" : 13,
+    	"codeName" : "Unauthorized",
+    	"$clusterTime" : {
+    		"clusterTime" : 0,
+    		"signature" : {
+    			"hash" : 0,
+    			"keyId" : 0
+    		}
+    	}
+    }
+    EOT
+  end
+
   describe '.sanitize' do
     it 'returns a valid json' do
       sanitized_json = described_class.sanitize(bson_data)
@@ -61,6 +100,15 @@ describe Puppet::Util::MongodbOutput do
     it 'replaces data types' do
       sanitized_json = described_class.sanitize(bson_data)
       expect(JSON.parse(sanitized_json)).to include(JSON.parse(json_data))
+    end
+
+    it 'extracts json from a corrupted output' do
+      sanitized_json = described_class.sanitize(corrupted_output)
+      expect(JSON.parse(sanitized_json)).to eq(JSON.parse(corrected_corrupted_output))
+    end
+
+    it 'returns string as is if no json there' do
+      expect(described_class.sanitize('3.6.3')).to eq('3.6.3')
     end
   end
 end
