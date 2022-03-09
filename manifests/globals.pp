bf6d48257b585be3f884a7ed31235803c372b0f0
Dennis Pohlmann <pohlmann.dennis@guj.de>
2022-03-09 15:20:29 +0100

    allow user to disable repo creation

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -43,18 +43,18 @@ class mongodb::globals (
   }
 
   # Setup of the repo only makes sense globally, so we are doing it here.
-  case $facts['os']['family'] {
-    'RedHat', 'Linux', 'Suse': {
-      class { 'mongodb::repo':
-        ensure              => present,
-        version             => pick($version, '3.6'),
-        use_enterprise_repo => $use_enterprise_repo,
-        repo_location       => $repo_location,
-        proxy               => $repo_proxy,
+  if $manage_package_repo {
+    case $facts['os']['family'] {
+      'RedHat', 'Linux', 'Suse': {
+        class { 'mongodb::repo':
+          ensure              => present,
+          version             => pick($version, '3.6'),
+          use_enterprise_repo => $use_enterprise_repo,
+          repo_location       => $repo_location,
+          proxy               => $repo_proxy,
+        }
       }
-    }
-    default: {
-      if $manage_package_repo {
+      default: {
         if $use_enterprise_repo == true and $version == undef {
           fail('You must set mongodb::globals::version when mongodb::globals::use_enterprise_repo is true')
         }
