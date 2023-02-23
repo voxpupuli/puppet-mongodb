859eb7e3ccc0540a7c2299ce2bdcfa3ed443df0a
Allan Briffa <allan.briffa@wandera.com>
2023-02-23 09:37:54 +0000

    Add repo GPG keys for newer mongo versions

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -57,6 +57,9 @@ class mongodb::repo (
           default => undef
         }
         $key = "${mongover[0]}.${mongover[1]}" ? {
+          '6.0'   => '39BD841E4BE5FB195A65400E6A26B1AE64C3C388',
+          '5.0'   => 'F5679A222C647C87527C2F8CB00A0BD1E2C63C11',
+          '4.4'   => '20691EEC35216C63CAF66CE1656408E390CFB1F5',
           '4.2'   => 'E162F504A20CDF15827F718D4B7C549A058F8B6B',
           '4.0'   => '9DA31620334BD75D9DCB49F368818C72E52529D4',
           '3.6'   => '2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5',
