6537f8b85a3ca5a77276c2575c37a43c7e70e927
Steve Traylen <steve.traylen@cern.ch>
2023-11-10 13:41:21 +0100

    Use facterdb with db_spec file
    With OS facts set the following error occured:
    
    ```
    NoMethodError:
         #   undefined method `downcase' for nil:NilClass
         #   ./vendor/bundle/ruby/3.2.0/gems/puppet-8.3.1/lib/puppet/provider/service/init.rb:25:in `block in <top (required)>'
    ```
    
    since the service provider could not be determined correctly.

--- spec/defines/db_spec.rb
+++ spec/defines/db_spec.rb
@@ -6,77 +6,85 @@ describe 'mongodb::db' do
   context 'default' do
     let(:title) { 'testdb' }
 
-    let(:params) do
-      { 'user'     => 'testuser',
-        'password' => 'testpass' }
-    end
+    on_supported_os.each do |os, os_facts|
+      context "on #{os}" do
+        let(:facts) do
+          os_facts
+        end
 
-    it 'contains mongodb_database with mongodb::server requirement' do
-      is_expected.to contain_mongodb_database('testdb')
-    end
+        let(:params) do
+          { 'user'     => 'testuser',
+            'password' => 'testpass' }
+        end
 
-    it 'contains mongodb_user with mongodb_database requirement' do
-      is_expected.to contain_mongodb_user('User testuser on db testdb'). \
-        with_username('testuser'). \
-        with_database('testdb'). \
-        that_requires('Mongodb_database[testdb]')
-    end
+        it 'contains mongodb_database with mongodb::server requirement' do
+          is_expected.to contain_mongodb_database('testdb')
+        end
 
-    it 'contains mongodb_user with proper roles' do
-      params['roles'] = %w[testrole1 testrole2]
-      is_expected.to contain_mongodb_user('User testuser on db testdb'). \
-        with_roles(%w[testrole1 testrole2])
-    end
+        it 'contains mongodb_user with mongodb_database requirement' do
+          is_expected.to contain_mongodb_user('User testuser on db testdb'). \
+            with_username('testuser'). \
+            with_database('testdb'). \
+            that_requires('Mongodb_database[testdb]')
+        end
 
-    it 'prefers password_hash instead of password' do
-      params['password_hash'] = 'securehash'
-      is_expected.to contain_mongodb_user('User testuser on db testdb'). \
-        with_password_hash('securehash')
-    end
+        it 'contains mongodb_user with proper roles' do
+          params['roles'] = %w[testrole1 testrole2]
+          is_expected.to contain_mongodb_user('User testuser on db testdb'). \
+            with_roles(%w[testrole1 testrole2])
+        end
 
-    it 'contains mongodb_database with proper tries param' do
-      params['tries'] = 5
-      is_expected.to contain_mongodb_database('testdb').with_tries(5)
-    end
-  end
+        it 'prefers password_hash instead of password' do
+          params['password_hash'] = 'securehash'
+          is_expected.to contain_mongodb_user('User testuser on db testdb'). \
+            with_password_hash('securehash')
+        end
 
-  context 'with a db_name value' do
-    let(:title) { 'testdb-title' }
+        it 'contains mongodb_database with proper tries param' do
+          params['tries'] = 5
+          is_expected.to contain_mongodb_database('testdb').with_tries(5)
+        end
+      end
 
-    let(:params) do
-      {
-        'db_name'  => 'testdb',
-        'user'     => 'testuser',
-        'password' => 'testpass'
-      }
-    end
+      context 'with a db_name value' do
+        let(:title) { 'testdb-title' }
 
-    it 'contains mongodb_database with mongodb::server requirement' do
-      is_expected.to contain_mongodb_database('testdb')
-    end
+        let(:params) do
+          {
+            'db_name'  => 'testdb',
+            'user'     => 'testuser',
+            'password' => 'testpass'
+          }
+        end
 
-    it 'contains mongodb_user with mongodb_database requirement' do
-      is_expected.to contain_mongodb_user('User testuser on db testdb'). \
-        with_username('testuser'). \
-        with_database('testdb'). \
-        that_requires('Mongodb_database[testdb]')
-    end
+        it 'contains mongodb_database with mongodb::server requirement' do
+          is_expected.to contain_mongodb_database('testdb')
+        end
 
-    it 'contains mongodb_user with proper roles' do
-      params['roles'] = %w[testrole1 testrole2]
-      is_expected.to contain_mongodb_user('User testuser on db testdb'). \
-        with_roles(%w[testrole1 testrole2])
-    end
+        it 'contains mongodb_user with mongodb_database requirement' do
+          is_expected.to contain_mongodb_user('User testuser on db testdb'). \
+            with_username('testuser'). \
+            with_database('testdb'). \
+            that_requires('Mongodb_database[testdb]')
+        end
 
-    it 'prefers password_hash instead of password' do
-      params['password_hash'] = 'securehash'
-      is_expected.to contain_mongodb_user('User testuser on db testdb'). \
-        with_password_hash('securehash')
-    end
+        it 'contains mongodb_user with proper roles' do
+          params['roles'] = %w[testrole1 testrole2]
+          is_expected.to contain_mongodb_user('User testuser on db testdb'). \
+            with_roles(%w[testrole1 testrole2])
+        end
 
-    it 'contains mongodb_database with proper tries param' do
-      params['tries'] = 5
-      is_expected.to contain_mongodb_database('testdb').with_tries(5)
+        it 'prefers password_hash instead of password' do
+          params['password_hash'] = 'securehash'
+          is_expected.to contain_mongodb_user('User testuser on db testdb'). \
+            with_password_hash('securehash')
+        end
+
+        it 'contains mongodb_database with proper tries param' do
+          params['tries'] = 5
+          is_expected.to contain_mongodb_database('testdb').with_tries(5)
+        end
+      end
     end
   end
 end
