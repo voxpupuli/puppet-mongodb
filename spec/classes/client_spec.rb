26db51530f2656fa7380ce00b50e3dffd08ca7a9
Hugo Haakseth <hugo.haakseth@spk.no>
2024-03-14 00:19:31 +0100

    bump default repo version to 4.4

--- spec/classes/client_spec.rb
+++ spec/classes/client_spec.rb
@@ -11,7 +11,7 @@ describe 'mongodb::client' do
         it { is_expected.to compile.with_all_deps }
 
         if facts[:os]['release']['major'] =~ %r{(10)}
-          it { is_expected.to create_package('mongodb_client').with_ensure('4.4.8') }
+          it { is_expected.to create_package('mongodb_client').with_ensure('4.4.29') }
         else
           it { is_expected.to create_package('mongodb_client').with_ensure('present') }
         end
@@ -25,7 +25,7 @@ describe 'mongodb::client' do
         it { is_expected.to compile.with_all_deps }
 
         if facts[:os]['release']['major'] =~ %r{(10)}
-          it { is_expected.to create_package('mongodb_client').with_ensure('4.4.8').with_name('mongodb-org-shell').with_tag('mongodb_package') }
+          it { is_expected.to create_package('mongodb_client').with_ensure('4.4.29').with_name('mongodb-org-shell').with_tag('mongodb_package') }
         else
           it { is_expected.to create_package('mongodb_client').with_ensure('present').with_name('mongodb-org-shell').with_tag('mongodb_package') }
         end
