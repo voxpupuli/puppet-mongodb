852a478135280b8c54e25600172188dd4d479063
Steven POST <steven.post@sfpd.fgov.be>
2024-04-02 14:51:35 +0200

    Prefer connecting over localhost when possible
    The `mongosh` command will fail to initiate a replicaset due to
    authentication, even when auth is disabled.

--- spec/unit/puppet/provider/mongodb_replset/mongodb_spec.rb
+++ spec/unit/puppet/provider/mongodb_replset/mongodb_spec.rb
@@ -43,7 +43,7 @@ describe Puppet::Type.type(:mongodb_replset).provider(:mongo) do
       allow(provider.class).to receive(:replset_properties)
       allow(provider).to receive(:get_hosts_status).and_return([valid_members, []])
       allow(provider).to receive(:master_host).and_return(false)
-      allow(provider).to receive(:rs_initiate).with('{"_id":"rs_test","members":[{"host":"mongo1:27017","_id":0},{"host":"mongo2:27017","_id":1},{"host":"mongo3:27017","_id":2}],"settings":{}}', 'mongo1:27017').and_return('info' => 'Config now saved locally.  Should come online in about a minute.', 'ok' => 1)
+      allow(provider).to receive(:rs_initiate).with('{"_id":"rs_test","members":[{"host":"mongo1:27017","_id":0},{"host":"mongo2:27017","_id":1},{"host":"mongo3:27017","_id":2}],"settings":{}}').and_return('info' => 'Config now saved locally.  Should come online in about a minute.', 'ok' => 1)
       allow(provider).to receive(:db_ismaster).and_return('{"ismaster" : true}')
       provider.create
       provider.flush
