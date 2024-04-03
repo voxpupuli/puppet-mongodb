d12656903b6a6e794da3701269f8fe07e5d242c8
Hugo Haakseth <hugo.haakseth@spk.no>
2024-04-03 13:12:20 +0200

    Add mongodb 6.0 and 7.0 to acceptance testing

--- spec/support/acceptance/supported_version.rb
+++ spec/support/acceptance/supported_version.rb
@@ -3,11 +3,15 @@
 def supported_version?(platform, version)
   return true if version.nil?
 
-  supported_versions = %w[4.4 5.0]
+  supported_versions = %w[4.4 5.0 6.0 7.0]
   return false unless supported_versions.include?(version)
 
   supported_versions.reject! do |v|
-    v < '5.0' && platform.start_with?('debian-11')
+    (v < '6.0' && platform.start_with?('el-9')) ||
+      (v > '6.0' && platform.start_with?('debian-10')) ||
+      (v < '5.0' && platform.start_with?('debian-11')) ||
+      (v < '7.0' && platform.start_with?('debian-12')) ||
+      (v < '6.0' && platform.start_with?('ubuntu-22'))
   end
 
   supported_versions.include?(version)
