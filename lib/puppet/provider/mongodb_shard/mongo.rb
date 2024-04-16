55c52f1176090cd1272643f8d8be45687c428e01
Hugo Haakseth <hugo.haakseth@spk.no>
2024-04-16 11:16:50 +0200

    Fix sharding acceptance testing

--- lib/puppet/provider/mongodb_shard/mongo.rb
+++ lib/puppet/provider/mongodb_shard/mongo.rb
@@ -102,7 +102,7 @@ Puppet::Type.type(:mongodb_shard).provide(:mongo, parent: Puppet::Provider::Mong
 
   def self.shard_properties(shard)
     properties = {}
-    output = mongo_command('sh.status()')
+    output = mongo_command('sh.status()')['value']
     output['shards'].each do |s|
       next unless s['_id'] == shard
 
@@ -118,7 +118,7 @@ Puppet::Type.type(:mongodb_shard).provide(:mongo, parent: Puppet::Provider::Mong
   end
 
   def self.shards_properties
-    output = mongo_command('sh.status()')
+    output = mongo_command('sh.status()')['value']
     properties = if output['shards'].empty?
                    []
                  else
@@ -163,64 +163,6 @@ Puppet::Type.type(:mongodb_shard).provide(:mongo, parent: Puppet::Provider::Mong
       retry
     end
 
-    # NOTE: (spredzy) : sh.status()
-    # does not return a json stream
-    # we jsonify it so it is easier
-    # to parse and deal with it
-    if command == 'sh.status()'
-      myarr = output.split("\n")
-      myarr.shift
-      myarr.pop
-      myarr.pop
-      final_stream = []
-      prev_line = nil
-      in_shard_list = 0
-      in_chunk = 0
-      myarr.each do |line|
-        line.gsub!(%r{sharding version:}, '{ "sharding version":')
-        line.gsub!(%r{shards:}, ',"shards":[')
-        line.gsub!(%r{databases:}, '], "databases":[')
-        line.gsub!(%r{"clusterId" : ObjectId\("(.*)"\)}, '"clusterId" : "ObjectId(\'\1\')"')
-        line.gsub!(%r{\{  "_id" :}, ',{  "_id" :') if %r{_id} =~ prev_line
-        # Modification for shard
-        line = '' if line =~ %r{on :.*Timestamp}
-        if line =~ %r{_id} && in_shard_list == 1
-          in_shard_list = 0
-          last_line = final_stream.pop.strip
-          proper_line = "#{last_line}]},"
-          final_stream << proper_line
-        end
-        if line =~ %r{shard key} && in_shard_list == 1
-          shard_name = final_stream.pop.strip
-          proper_line = ",{\"#{shard_name}\":"
-          final_stream << proper_line
-        end
-        if line =~ %r{shard key} && in_shard_list.zero?
-          in_shard_list = 1
-          shard_name = final_stream.pop.strip
-          id_line = "#{final_stream.pop[0..-2]}, \"shards\": "
-          proper_line = "[{\"#{shard_name}\":"
-          final_stream << id_line
-          final_stream << proper_line
-        end
-        if in_chunk == 1
-          in_chunk = 0
-          line = "\"#{line.strip}\"}}"
-        end
-        in_chunk = 1 if line =~ %r{chunks} && in_chunk.zero?
-        line.gsub!(%r{shard key}, '{"shard key"')
-        line.gsub!(%r{chunks}, ',"chunks"')
-        final_stream << line unless line.empty?
-        prev_line = line
-      end
-      final_stream << ' ] }' if in_shard_list == 1
-      final_stream << ' ] }'
-      output = final_stream.join("\n")
-    end
-
-    # Hack to avoid non-json empty sets
-    output = '{}' if output == "null\n"
-    output.gsub!(%r{\s*}, '')
     JSON.parse(output)
   end
 end
