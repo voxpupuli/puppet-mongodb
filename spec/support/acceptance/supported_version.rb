2b6d543d1add1cebb47367643eb94cfe4d132721
Hugo Haakseth <hugo.haakseth@spk.no>
2025-06-06 00:55:22 +0200

    remove tests on EOL MongoDB 5.x

--- spec/support/acceptance/supported_version.rb
+++ spec/support/acceptance/supported_version.rb
@@ -3,15 +3,11 @@
 def supported_version?(platform, version)
   return true if version.nil?
 
-  supported_versions = %w[5.0 6.0 7.0]
+  supported_versions = %w[6.0 7.0]
   return false unless supported_versions.include?(version)
 
   supported_versions.reject! do |v|
-    (v < '6.0' && platform.start_with?('el-9')) ||
-      (v > '6.0' && platform.start_with?('debian-10')) ||
-      (v < '5.0' && platform.start_with?('debian-11')) ||
-      (v < '7.0' && platform.start_with?('debian-12')) ||
-      (v < '6.0' && platform.start_with?('ubuntu-22'))
+    (v < '7.0' && platform.start_with?('debian-12'))
   end
 
   supported_versions.include?(version)
