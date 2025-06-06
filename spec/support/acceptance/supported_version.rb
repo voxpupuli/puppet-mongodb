1e8548c88ec80329318c59ea982503a215407885
Hugo Haakseth <hugo.haakseth@spk.no>
2025-06-06 15:00:05 +0200

    Add MongoDB 8.x to integration tests

--- spec/support/acceptance/supported_version.rb
+++ spec/support/acceptance/supported_version.rb
@@ -3,11 +3,13 @@
 def supported_version?(platform, version)
   return true if version.nil?
 
-  supported_versions = %w[6.0 7.0]
+  supported_versions = %w[6.0 7.0 8.0]
   return false unless supported_versions.include?(version)
 
   supported_versions.reject! do |v|
-    (v < '7.0' && platform.start_with?('debian-12'))
+    (v > '7.0' && platform.start_with?('debian-11')) ||
+      (v < '7.0' && platform.start_with?('debian-12')) ||
+      (v < '8.0' && platform.start_with?('ubuntu-24'))
   end
 
   supported_versions.include?(version)
